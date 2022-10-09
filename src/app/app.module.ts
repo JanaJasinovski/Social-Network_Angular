eda4fd7a70485d6538146331b599038e8e008104
jana <jantschick3@gmail.com>
2022-10-09 17:56:04 +0300

    3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MaterialModule} from "./material-module";
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {authInterceptorProviders} from "./helper/auth-interceptor.service";
 import {ErrorInterceptorService} from "./helper/error-interceptor.service";
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {NavigationComponent} from './layout/navigation/navigation.component';
+import {IndexComponent} from './layout/index/index.component';
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {ProfileComponent} from './user/profile/profile.component';
+import {UserPostsComponent} from './user/user-posts/user-posts.component';
+import {EditUserComponent} from './user/edit-user/edit-user.component';
+import {AddPostComponent} from './user/add-post/add-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavigationComponent,
+    IndexComponent,
+    ProfileComponent,
+    UserPostsComponent,
+    EditUserComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
@@ -25,9 +38,11 @@ import { RegisterComponent } from './auth/register/register.component';
     ReactiveFormsModule,
     MaterialModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatTooltipModule
   ],
   providers: [authInterceptorProviders, ErrorInterceptorService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/layout/navigation/navigation.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {TokenStorageService} from "../../service/token-storage.service";
+import {UserService} from "../../service/user.service";
+import {User} from "../../moduls/User";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+
+  isLoggedIn = false;
+  isDataLoaded = false;
+  user!: User;
+
+  constructor(private tokenService: TokenStorageService,
+              private userService: UserService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenService.getToken();
+    if(this.isLoggedIn) {
+      this.userService.getCurrentUser()
+        .subscribe( data => {
+          this.user = data;
+          this.isDataLoaded = true;
+        })
+    }
+  }
+
+  logout(): void {
+    this.tokenService.logout();
+    this.router.navigate(['/login']);
+  }
+}
